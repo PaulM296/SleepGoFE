cef3a6fd86bd16c7711e2142561e06f535d8a428
PaulM <paulmicluta@gmail.com>
2023-06-10 20:36:43 +0300

    Id removed

--- src/app/user-account/user-account.component.ts
+++ src/app/user-account/user-account.component.ts
@@ -23,7 +23,6 @@ export class UserAccountComponent implements OnInit {
   constructor(private formBuilder: FormBuilder, private apiService: ApiService, private router: Router,
               private activatedRoute: ActivatedRoute, private snackBar: MatSnackBar, private dialog: MatDialog) {
     this.profileForm = this.formBuilder.group({
-      id: ['', Validators.required],
       firstName: ['', Validators.required],
       lastName: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
@@ -43,7 +42,6 @@ export class UserAccountComponent implements OnInit {
       .subscribe({
         next: (user: UserModel) => {
           this.profileForm.patchValue({
-            id: user.id,
             firstName: user.firstName,
             lastName: user.lastName,
             email: user.email,
